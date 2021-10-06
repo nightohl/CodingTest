2674d0fa9b1deffc598935b33ddbff773f134b15
nightohl <haneojin@gmail.com>
2021-10-07 03:47:44 +0900

    quiz: 괄호_변환

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import {메뉴_리뉴얼_테스트, 합승_택시_테스트} from './2021-kakao-blind-recruitment/index.js';
-
+import {괄호_변환_테스트} from './2020-kakao-blind-recruitment/index.js'
 // 메뉴_리뉴얼_테스트();
-합승_택시_테스트();
+// 합승_택시_테스트();
+괄호_변환_테스트();
